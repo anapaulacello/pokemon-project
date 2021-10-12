179abcc2221031a9f6e52793a983175f5d6772f4
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-12 16:32:11 +0200

    spiner arreglado

--- app.js
+++ app.js
@@ -82,10 +82,12 @@ randomBtn.addEventListener('click',randomPokemon);
 let displayBuscar=document.querySelector(".buscar");
 
 let buscarPokemon=async()=>{
-    spinner.style.display="block";
-    document.querySelector('.pokebal-gif').style.display="none";
-
     let id=document.querySelector(".buscar__input").value;
+    if (id>898) {
+        window.alert("solo hay 898 pokemons");
+        spinner.style.display="none";
+    }
+
     let result=await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     let pokemonToJson=await result.json();
     spinner.style.display="none";
@@ -101,6 +103,7 @@ let buscarPokemon=async()=>{
        <h2 class="displayBuscar__id">${pokemonInfo.id}</h2>
        <img class="displayBuscar__image" src="${pokemonInfo.image}" alt="${pokemonInfo.name}"/>`;
     displayBuscar.innerHTML=pokemonHTML;
+    document.querySelector('.pokebal-gif').style.display="none";
 }
 
 document.querySelector(".buscar__btn").addEventListener('click',buscarPokemon)
